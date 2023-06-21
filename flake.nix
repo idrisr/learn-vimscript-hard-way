c1c44cf53de9c86e8d139ba40108d442910d0854
Idris Raja <idris.raja@gmail.com>
2023-06-21 17:15:54 -0500

    up to chapter 15 operator pending

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
       mynvim = vimrc:
         pkgs.writeShellScriptBin "mynvim" ''
           # Call hello with a traditional greeting
-          exec ${pkgs.neovim}/bin/nvim -u ${vimrc}
+          exec ${pkgs.neovim}/bin/nvim -u ${vimrc} $@
         '';
       app = vimrc: {
         type = "app";
@@ -29,6 +29,11 @@
         ex01 = app ./01vimrc;
         ex03 = app ./03vimrc;
         ex04 = app ./04vimrc;
+        ex05 = app ./05vimrc;
+        ex07 = app ./07vimrc;
+        ex09 = app ./09vimrc;
+        ex13 = app ./13vimrc;
+        ex15 = app ./15vimrc;
       };
     };
 }
