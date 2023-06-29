3074e859f2578c87a06393383c828c9b71147142
Idris Raja <idris.raja@gmail.com>
2023-06-28 19:49:17 -0500

    get potion going on... barely

--- flake.nix
+++ flake.nix
@@ -3,15 +3,6 @@
   description = "messing around with vimscript";
   outputs = { self, nixpkgs, ... }:
     let
-      # potion = pkgs.stdenv.mkDerivation {
-      # name = "potion";
-      # src = pkgs.fetchFromGitHub {
-      # owner = "perl11";
-      # repo = "potion";
-      # rev = "0e9dc6583094282506e7a181008ffb245a77d6ce";
-      # sha256 = "P6BthLMa1Epn29BQp34JZLuTHFsCQVC+eAQeqVVK9zk=";
-      # };
-      # };
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
       mynvim = vimrc:
@@ -24,14 +15,67 @@
         description = "custom vimrc ${vimrc}";
         program = "${mynvim vimrc}/bin/mynvim";
       };
+      libuv = with pkgs;
+        stdenv.mkDerivation rec {
+          name = "libuv";
+          src = pkgs.fetchFromGitHub {
+            owner = "rurban";
+            repo = "libuv";
+            rev = "a88a777cb3bcb7a2fa59c3a19ef8fcf0e06eb38e";
+            sha256 = "sha256-GhDpnU7iBvXKj18+xPOLM2XraQahbkboRxH8nBnw1ZM=";
+          };
+          buildInputs = [ libtool automake autoconf ];
+          preConfigure = "${bash}/bin/bash autogen.sh";
+          configureFlags = [ "--enable-shared" ];
+        };
+
+      potion = with pkgs;
+        stdenv.mkDerivation rec {
+          name = "potion";
+          src = pkgs.fetchFromGitHub {
+            owner = "perl11";
+            repo = "potion";
+            rev = "0e9dc6583094282506e7a181008ffb245a77d6ce";
+            sha256 = "P6BthLMa1Epn29BQp34JZLuTHFsCQVC+eAQeqVVK9zk=";
+          };
+          preConfigure = "cp ${./version.h} core/version.h";
+          dontAddPrefix = true;
+          configurePhase = ''
+            cp ${./version.h} core/version.h;
+            ./configure
+          '';
+          postPatch = ''
+            substituteInPlace ./Makefile ./config.mak \
+            --replace /bin/echo ${coreutils}/bin/echo \
+            --replace /bin/cat ${coreutils}/bin/cat \
+            --replace /bin/mv ${coreutils}/bin/mv \
+            --replace 'RPATH         = -Wl,-rpath=''${PWD}/lib' 'RPATH = -Wl,-rpath=$(out)/lib'
+          '';
+          patches = [ ./make.patch ./config.mak.patch ];
+          buildInputs = [ makeWrapper readline libuv git perl ];
+          nativeBuildInputs = [ readline libuv git perl ];
+          installPhase = ''
+            mkdir -p $out/{bin,lib}
+            patchelf --add-needed "readline.so" bin/potion
+            cp bin/potion $out/bin
+            wrapProgram $out/bin/potion --set LD_LIBRARY_PATH "$out/lib"
+            cp lib/libpotion.so $out/lib
+            cp lib/potion/readline.so $out/lib/readline.so
+          '';
+        };
 
     in {
-      devShells.${system}.default = pkgs.mkShell {
-        buildInputs = with pkgs; [
-          luajitPackages.lua-lsp
-          nodePackages.vim-language-server
-        ];
+      devShells.${system} = {
+        default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            luajitPackages.lua-lsp
+            nodePackages.vim-language-server
+          ];
+        };
+        inherit potion;
       };
+
+      packages.${system} = { inherit potion; };
 
       # devShells.${system}.default = potion;
 
---
+++ version.h
@@ -0,0 +1,7 @@
+/* created by  -f config.mak */
+#define POTION_MAJOR    0
+#define POTION_MINOR    3
+#define POTION_VERSION "0.3"
+#define POTION_DATE    "2023-06-25"
+#define POTION_COMMIT  "28d207b"
+#define POTION_REV     "1312"
