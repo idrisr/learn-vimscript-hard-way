8fc7623ce3b1b0525148979d17f29796d3a2c691
Idris Raja <idris.raja@gmail.com>
2023-06-29 10:54:55 -0500

    get the plugin going on

--- flake.nix
+++ flake.nix
@@ -5,15 +5,33 @@
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
-      mynvim = vimrc:
-        pkgs.writeShellScriptBin "mynvim" ''
-          # call neovim with custom config
-          exec ${pkgs.neovim}/bin/nvim -u ${vimrc} "$@"
+
+      mynvim = with pkgs;
+        vimrc:
+        runCommand "test" {
+          nativeBuildInputs = [ makeWrapper neovim-unwrapped ];
+        } ''
+          mkdir -p $out/bin
+          cp ${neovim-unwrapped}/bin/nvim $out/bin
+          wrapProgram $out/bin/nvim --add-flags '-u "./vimrc/${vimrc}"' --set PATH ${neovim}/bin/path
         '';
+
+      nvim-plugin = with pkgs;
+        vimrc:
+        runCommand "test" {
+          nativeBuildInputs = [ makeWrapper neovim plugin ];
+        } ''
+          mkdir -p $out/bin
+          cp ${neovim}/bin/nvim $out/bin/
+          cp ${vimrc} $out/bin/vimrc
+          substituteInPlace $out/bin/vimrc --subst-var-by pluginPath "${plugin}"
+          wrapProgram $out/bin/nvim --add-flags "-u $out/bin/vimrc" --set PATH ${neovim}/bin/path
+        '';
+
       app = vimrc: {
         type = "app";
         description = "custom vimrc ${vimrc}";
-        program = "${mynvim vimrc}/bin/mynvim";
+        program = "${mynvim vimrc}/bin/nvim";
       };
 
       potion = with pkgs;
@@ -41,7 +59,7 @@
           preConfigure = "cp ${./version.h} core/version.h";
           dontAddPrefix = true;
           configurePhase = ''
-            cp ${./version.h} core/version.h;
+            runHook preConfigure
             ./configure
           '';
           patches = [ ./make.patch ./config.mak.patch ];
@@ -52,7 +70,7 @@
             --replace /bin/mv ${coreutils}/bin/mv \
             --replace 'RPATH         = -Wl,-rpath=''${PWD}/lib' 'RPATH = -Wl,-rpath=$(out)/lib'
           '';
-          buildInputs = [ makeWrapper perl ];
+          buildInputs = [ git makeWrapper perl ];
           nativeBuildInputs = [ libuv ];
           installPhase = ''
             mkdir -p $out/{bin,lib}
@@ -62,6 +80,13 @@
             cp lib/libpotion.so $out/lib
             cp lib/potion/readline.so $out/lib/readline.so
           '';
+        };
+
+      plugin = with pkgs;
+        vimUtils.buildVimPluginFrom2Nix {
+          pname = "vim-potion";
+          version = "2021-11-21";
+          src = ./plugin;
         };
 
     in {
@@ -74,39 +99,46 @@
         };
         inherit potion;
       };
+      packages.${system} = {
+        inherit plugin;
+        ex01 = mynvim ./vimrc/01vimrc.vim;
+        ex44 = nvim-plugin ./vimrc/44vimrc.vim;
+      };
 
-      packages.${system} = { inherit potion; };
-
-      # devShells.${system}.default = potion;
-
-      apps.${system} = {
-        ex01 = app ./01vimrc.vim;
-        ex03 = app ./03vimrc.vim;
-        ex04 = app ./04vimrc.vim;
-        ex05 = app ./05vimrc.vim;
-        ex07 = app ./07vimrc.vim;
-        ex09 = app ./09vimrc.vim;
-        ex13 = app ./13vimrc.vim;
-        ex15 = app ./15vimrc.vim;
-        ex16 = app ./16vimrc.vim;
-        ex19 = app ./19vimrc.vim;
-        ex21 = app ./21vimrc.vim;
-        ex22 = app ./22vimrc.vim;
-        ex23 = app ./23vimrc.vim;
-        ex24 = app ./24vimrc.vim;
-        ex25 = app ./25vimrc.vim;
-        ex26 = app ./26vimrc.vim;
-        ex27 = app ./27vimrc.vim;
-        ex28 = app ./28vimrc.vim;
-        ex29 = app ./29vimrc.vim;
-        ex30 = app ./30vimrc.vim;
-        ex31 = app ./31vimrc.vim;
-        ex32 = app ./32vimrc.vim;
-        ex33 = app ./33vimrc.vim;
-        ex34 = app ./34vimrc.vim;
-        ex35 = app ./35vimrc.vim;
-        ex37 = app ./37vimrc.vim;
-        ex38 = app ./38vimrc.vim;
+      apps.${system} = let x = "01";
+      in {
+        "ex${x}" = app "${x}vimrc.vim";
+        ex03 = app "03vimrc.vim";
+        ex04 = app "04vimrc.vim";
+        ex05 = app "05vimrc.vim";
+        ex07 = app "07vimrc.vim";
+        ex09 = app "09vimrc.vim";
+        ex13 = app "13vimrc.vim";
+        ex15 = app "15vimrc.vim";
+        ex16 = app "16vimrc.vim";
+        ex19 = app "19vimrc.vim";
+        ex21 = app "21vimrc.vim";
+        ex22 = app "22vimrc.vim";
+        ex23 = app "23vimrc.vim";
+        ex24 = app "24vimrc.vim";
+        ex25 = app "25vimrc.vim";
+        ex26 = app "26vimrc.vim";
+        ex27 = app "27vimrc.vim";
+        ex28 = app "28vimrc.vim";
+        ex29 = app "29vimrc.vim";
+        ex30 = app "30vimrc.vim";
+        ex31 = app "31vimrc.vim";
+        ex32 = app "32vimrc.vim";
+        ex33 = app "33vimrc.vim";
+        ex34 = app "34vimrc.vim";
+        ex35 = app "35vimrc.vim";
+        ex37 = app "37vimrc.vim";
+        ex38 = app "38vimrc.vim";
+        ex44 = {
+          type = "app";
+          program = let package = nvim-plugin ./vimrc/44vimrc.vim;
+          in "${package}/bin/nvim";
+        };
       };
     };
 }
